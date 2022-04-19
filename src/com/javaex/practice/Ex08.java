928ec2cffbbe88f64984421a3bae3c8886bcb57b
경애 <@DESKTOP-L1K3QFL>
2022-04-19 09:55:19 +0900

    2022-04-18 practice02_2

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -5,9 +5,9 @@ public class Ex08 {
 	public static void main(String[] args) {
 
 
-		for(int dan=2; dan<=9; dan++) {
+		for(int i=1; i<=9; i++) {
 			
-			for(int i=1; i<=9; i++) {
+			for(int dan=2; dan<=9; dan++) {
 				
 				System.out.print(dan+"*"+i+"="+(dan*i)+"\t");
 			}
