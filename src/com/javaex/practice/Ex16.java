5cd1d6d245f5125da98e4f3bbd9507dccd404cb5
경애 <@DESKTOP-L1K3QFL>
2022-04-19 16:19:40 +0900

    2022-04-19 pratice03_03

--- src/com/javaex/practice/Ex16.java
+++ src/com/javaex/practice/Ex16.java
@@ -9,24 +9,43 @@ public class Ex16 {
 		Scanner sc=new Scanner(System.in);
 		int sum=0;
 		int count=0;
-		
+
 		System.out.print("숫자를 입력하세요: ");
 		int num = sc.nextInt();
-		
-		for(int a=5; a<=num; a=a+5) {
-			count=count+1;
-		}
-		System.out.println("5의 배수의 개수: "+count);
-		
+
 		for(int i=5; i<=num; i=i+5) {
+			count=count+1;
 			sum=sum+i;
 		}
+		System.out.println("5의 배수의 개수: "+count);
 		System.out.println("5의 배수의 합: "+sum);
-		
-		
+	
 		sc.close();
 		
 
 	}
 
 }
+
+
+
+
+/*
+int sum=0;
+int count=0;
+
+System.out.print("숫자를 입력하세요: ");
+int num = sc.nextInt();
+
+for(int a=5; a<=num; a=a+5) {
+	count=count+1;
+	
+}
+System.out.println("5의 배수의 개수: "+count);
+
+for(int i=5; i<=num; i=i+5) {
+	sum=sum+i;
+}
+System.out.println("5의 배수의 합: "+sum);
+
+*/
