29ec0a3e2ead9e90daa8d096ec54f4c974c084b8
경애 <@DESKTOP-L1K3QFL>
2022-04-19 09:53:31 +0900

    2022-04-18 practice03_2

--- src/com/javaex/practice/Ex09.java
+++ src/com/javaex/practice/Ex09.java
@@ -5,7 +5,7 @@ public class Ex09 {
 	public static void main(String[] args) {
 		
 		for(int y=1; y<=10; y++) {
-			for(int x=y; x<=10+y; x++) {
+			for(int x=y; x<=9+y; x++) {
 				
 				System.out.print(x+"\t");
 				
